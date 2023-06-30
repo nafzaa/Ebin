5573221c614bcf7ba029a08e234d21c309222b30
Akif <akif.frees@gmail.com>
2023-06-30 23:56:41 +0800

    add dashboard UI

--- resources/js/components/Dashboard.jsx
+++ resources/js/components/Dashboard.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Total from "./Tong/Total";
+import Instock from "./Tong/Instock";
+import Outstock from "./Tong/Outstock";
 
 const Dashboard = () => {
-    return <div>Dashboard</div>;
+    return(
+    <div>
+        <h2>Tong</h2>
+        <section class="section dashboard">
+            <div class="row">
+                <Total/>
+                <Instock/>
+                <Outstock/>
+            </div>
+        </section>
+    </div>
+    )
 };
 
 export default Dashboard;
